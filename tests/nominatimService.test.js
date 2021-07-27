880bb783d9a72e9c2f6d1d3a31328a3b3829229e
Maxime <maxrofra@gmail.com>
2021-07-27 22:05:08 +0200

    Setup unit testing environment

---
+++ tests/nominatimService.test.js
@@ -0,0 +1,11 @@
+
+// Written by TDD
+
+
+// TESTS
+// check server status == valid
+// search for location 1
+// search for location 2
+// search for location 3
+// failure case: catch invalid input
+// failure case: catch invalid city
