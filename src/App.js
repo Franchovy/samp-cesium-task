5a15c53dcd90616fb2db24922a147f5fbd6e6f6e
Maxime <maxrofra@gmail.com>
2021-07-28 17:00:01 +0200

    Fixed setting user access token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import BillboardMaker from "./billboardMaker";
-import Cesium from 'cesium';
-
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMmJkMTYyMS0wOGJlLTRmNWUtOTk2Ny00OTVmODVhMTAzMDEiLCJpZCI6NjI2MjYsImlhdCI6MTYyNzMwNTU5OH0.s-HIL4Yept3UTh713lk_eGcom58I3Z3P6x1dhnsUpzQ';
 
 const App = () => {
   return <BillboardMaker />;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { Ion } from 'cesium';
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMmJkMTYyMS0wOGJlLTRmNWUtOTk2Ny00OTVmODVhMTAzMDEiLCJpZCI6NjI2MjYsImlhdCI6MTYyNzMwNTU5OH0.s-HIL4Yept3UTh713lk_eGcom58I3Z3P6x1dhnsUpzQ';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
