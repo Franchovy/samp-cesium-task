ad67fe22a1c2611a7f9947c8da9385542b81aaa3
Maxime <maxrofra@gmail.com>
2021-07-30 13:26:30 +0200

    Fixed testing setup

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 const jestConfig = {
-  clearMocks: true,
-  verbose: true,
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.{js,jsx,mjs}"],
+  testMatch: ["<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}", "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}"],
   transform: {
-    "\\.[jt]sx?$": "babel-jest",
+    "^.+\\.(js|jsx|mjs)$": "<rootDir>/config/jest/jest-transformer.js"
   },
-  testMatch: ["**/__tests__/*.js?(x)"],
-  setupFilesAfterEnv: ["./rtl.setup.js"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"]
 };
 
 module.exports = jestConfig;
